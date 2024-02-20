0d034185de1f9c60aae76b9e4b91ca281dda2ff5
Gleb Gurgurov <gurragyrra@gmail.com>
2024-02-20 14:50:48 +0200

    use env-cmd to hide secter info

--- backend/src/db/mongoose.js
+++ backend/src/db/mongoose.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose')
 
-const URL =
-	'mongodb+srv://Gleb:Essential98@ray.pr1rab9.mongodb.net/ray-api?retryWrites=true&w=majority'
+const url = process.env.URL_RAY_DB
 mongoose
-	.connect(URL)
+	.connect(url)
 	.then(res => console.log('connected to MongoDB'))
 	.catch(err => console.log(`error to connection: ${err}`))
